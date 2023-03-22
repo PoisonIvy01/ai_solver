1a7da694e4b4a3df3c7a5ba260478da119172c57
chris <chris@penguin>
2023-03-22 13:52:26 -0400

    add stuff

---
+++ testing.py
@@ -0,0 +1,28 @@
+import openai
+from PIL import Image
+import pytesseract
+import cv2
+
+img_cv = cv2.imread(r'document.png')
+
+#i have no idea what im doing wheeeeeeeeee
+#pytesseract.pytesseract.tesseract_cmd = r'full path to executable'
+
+def askGPT(text):
+    openai.api_key = "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"
+    response = openai.Completion.create(
+            engine = "text-davinci-003",
+            prompt = text,
+            temperature = 0.6,
+            max_tokens = 30,
+    )
+    return print(response.choices[0].text)
+
+def main():
+    myQuestion = input("GPT: Ask me a question with good grammar!\n")
+    askGPT(myQuestion + "Answer in a single sentence.")
+    img_rgb = cv2.cvtColor(img_cv, cv2.COLOR_BGR2RGB)
+    print(pytesseract.image_to_string(img_rgb))
+    print("finished")
+
+main()
